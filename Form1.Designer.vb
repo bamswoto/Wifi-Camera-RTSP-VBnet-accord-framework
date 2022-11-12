6f86c5f78f55323b6bc89d83da10746cb453a903
bams_ <bams_@MEGATROLL>
2022-11-12 15:50:13 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New Accord.Controls.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(898, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(27, 41)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(804, 516)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1049, 750)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As Accord.Controls.PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,39 @@
+﻿
+Imports System.Net.Http
+Imports Accord.Controls
+Imports Accord.Imaging.Filters
+Imports Accord.Video.FFMPEG
+
+Public Class Form1
+    Dim vidplay As New VideoSourcePlayer
+    Dim rtsp As String = "rtsp://192.168.8.102/live/ch00_1"
+    Dim ffmpeg As VideoFileSource = New VideoFileSource(rtsp)
+    Dim buffer As Bitmap
+    Dim stts_cam As Boolean
+
+    Sub run_buffer()
+        buffer = vidplay.GetCurrentVideoFrame
+        Dim abcdeg As Integer = 200
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.Image = buffer
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not stts_cam Then
+            stts_cam = True
+            Button1.Text = "ON"
+            vidplay.VideoSource = ffmpeg
+            vidplay.Start()
+            AddHandler Application.Idle, AddressOf run_buffer
+
+        Else
+            stts_cam = False
+            Button1.Text = "Off"
+            vidplay.WaitForStop()
+            vidplay.Stop()
+
+        End If
+    End Sub
+
+End Class
+
